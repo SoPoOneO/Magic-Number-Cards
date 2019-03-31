127594b7c6a210660aa74bd3ba9f14530528450c
SoPoOneO <jbussey365@yahoo.com>
2019-03-31 16:15:53 -0400

    updated with suggestions from Reddit LearnPython sub

--- create-magic-number-cards.py
+++ create-magic-number-cards.py
@@ -1,26 +1,29 @@
-def conv(num,b):
-    convStr = "0123456789"
+from string import ascii_uppercase, digits
+
+def main():
+    base = int(input("Numeric base? "))
+    dec_places = int(input("Decimal places? "))
+
+    high = base ** dec_places
+    card_sets = [[[] for x in range(base-1)] for y in range(dec_places)]
+    for x in range(1, high):
+        converted = convertBase(x, base).rjust(dec_places, '0')
+
+        for i, digit in enumerate(converted):
+            if digit != "0":
+                card_sets[i][int(digit)-1].append(x)
+
+    letter_index = 0
+    for card_set in card_sets:
+        for card in card_set:
+            print(ascii_uppercase[letter_index] + ": " + str(card))
+            letter_index += 1
+
+def convertBase(num,b):
     if num<b:
-        return convStr[num]
+        return digits[num]
     else:
-        return conv(num//b,b) + convStr[num%b]
+        return convertBase(num//b,b) + digits[num%b]
 
-base = int(raw_input("Numeric base? "))
-dec_places = int(raw_input("Decimal places? "))
-
-high = base ** dec_places
-card_sets = [[[] for x in range(base-1)] for y in range(dec_places)]
-for x in range(1, high):
-    converted = conv(x, base).rjust(dec_places, '0')
-
-    for i in range( len(converted) ):
-        dig = converted[i]
-        if dig != "0":
-            card_sets[i][int(dig)-1].append(x)
-
-letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-letter_index = 0
-for card_set in card_sets:
-    for card in card_set:
-        print letters[letter_index] + ": " + str(card)
-        letter_index += 1
+if __name__ == '__main__':
+   main()
